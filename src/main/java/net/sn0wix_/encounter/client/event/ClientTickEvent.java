193a84e2585da7dfc7fae1c845e71170a4524914
Sn0wix_ <zerochipmunk317@centrum.cz>
2024-01-28 21:16:42 +0100

    return to f1 when scaring

--- src/main/java/net/sn0wix_/encounter/client/event/ClientTickEvent.java
+++ src/main/java/net/sn0wix_/encounter/client/event/ClientTickEvent.java
@@ -2,8 +2,10 @@ package net.sn0wix_.encounter.client.event;
 
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.Perspective;
 import net.sn0wix_.encounter.client.keys.KeyInputHandler;
 import net.sn0wix_.encounter.client.util.ClientVariables;
+import net.sn0wix_.encounter.common.Encounter;
 
 public class ClientTickEvent {
     public static class EndClientTick implements ClientTickEvents.EndTick {
@@ -12,6 +14,10 @@ public class ClientTickEvent {
         public void onEndTick(MinecraftClient client) {
             stopMoving(client);
             KeyInputHandler.handleInputs(client);
+
+            if (ClientVariables.isPlayerLocked()) {
+                handleF5(client);
+            }
 
             if (client.player == null && ClientVariables.isPlayerLocked()) {
                 ClientVariables.unlockPlayer();
@@ -23,6 +29,14 @@ public class ClientTickEvent {
         @Override
         public void onStartTick(MinecraftClient client) {
             stopMoving(client);
+        }
+    }
+
+    public static void handleF5(MinecraftClient client) {
+        if (!client.options.getPerspective().isFirstPerson()) {
+            client.options.setPerspective(Perspective.FIRST_PERSON);
+            client.gameRenderer.onCameraEntitySet(client.getCameraEntity());
+            client.worldRenderer.scheduleTerrainUpdate();
         }
     }
 
